b6c432c1fb9e3c0c39d3a6508cd2a0261978b47f
Ricky noviansyah <60885444+codericky@users.noreply.github.com>
2021-01-03 13:38:53 +0700

    "add rust file"

---
+++ learn.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let name:&str="Ricky";
+   let address:&str = "Tangerang City";
+   let hobby:&str = "sports and music";
+
+   println!("my name : {} im from :{}, my hobby : {}",name,address,hobby);
+}
